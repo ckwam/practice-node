95062f54a2bf17073eb0f3dd3c46d34ec78f65a7
ckwam <ckwa@vivaldi.net>
2020-04-17 18:56:19 +0900

    small fix

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ const server = http.createServer((req, res) => {
             }).on('end', () => {
                 body = Buffer.concat(body).toString();
                 const decoded = decodeURIComponent(body);
-                console.info('[' + now + '] 投稿: ' + decoded);
+                console.info('選択: ' + decoded);
                 res.write('<!DOCTYPE html><html lang="ja"><body><h1>' +
                     decoded + 'が投稿されました</h1></body></html>');
                 res.end();
